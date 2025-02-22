582f004842799fb38b4c9d395cca0b0140f89465
Óscar Castellanos Rodríguez <oscar.castellanos@udc.es>
2025-02-22 13:38:13 +0100

    Clothing_detail model updated with new fields

--- clothing_compare/clothing_app/models.py
+++ clothing_compare/clothing_app/models.py
@@ -4,12 +4,23 @@ from django.db import models
 
 
 class Clothing_detail(models.Model):
+    # Basic data returned by Product Search
     clothing_id = models.CharField(max_length=20)
     name = models.CharField(max_length=50)
     price_currency = models.CharField(max_length=5)
-    price_current = models.IntegerField()
-    link = models.CharField(max_length=100)
+    price_current = models.CharField()
+    price_original = models.CharField()
+    #link = models.CharField(max_length=100)
+    link = models.URLField()
     brand = models.CharField(max_length=20)
+    # Scraped data
+    color = models.CharField(max_length=20)
+    description = models.TextField() # example: {nylon: 55%, cotton: 45%}
+    composition = models.JSONField(null=True)
+    image_url = models.URLField()
+    # Generated data
+    score = models.IntegerField(default=-1)
+
 
 class User_clothing(models.Model):
     user = models.CharField(max_length=20)
