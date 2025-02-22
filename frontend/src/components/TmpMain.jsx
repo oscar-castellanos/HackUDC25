9971c1d73726c84dae1204b890532fd5947c007f
Alejandro Fernandez-Fraga <a.fernandez3@udc.es>
2025-02-22 01:46:56 +0100

    modified capture Webcam picture component to write to the fake test api

--- frontend/src/components/TmpMain.jsx
+++ frontend/src/components/TmpMain.jsx
@@ -1,12 +1,34 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Webcam from './Webcam';
 
 const TmpMain = () => {
   const [capturedImage, setCapturedImage] = useState(null);
 
+  console.log("TmpMain called");
+  // When the capturedImage changes
+  // send a POST request to the backend
+  // Send first 50 characters of the image
+  useEffect(() => {
+    console.log("useEffect called");
+    if (capturedImage) {
+      console.log("Captured image changed");
+      fetch('http://localhost:8000/clothing_app/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ name: "ImgXXX",
+                                detail: capturedImage.substring(0, 50)}),
+      })
+        .then((response) => response.json())
+        .then((data) => console.log(data));
+    }
+  }
+  , [capturedImage]);
+
   return (
     <main>
-      <h1>Temp Main</h1>
+      <h1>Temp Mone</h1>
       <div className="webcam-container">
         {!capturedImage && <Webcam setCapturedImage={setCapturedImage} />}
         {capturedImage && (
