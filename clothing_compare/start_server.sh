e250fef50d9b274857c8a95c2dc1ed5c36de0d76
Óscar Castellanos Rodríguez <oscar.castellanos@udc.es>
2025-02-22 01:08:26 +0100

    Dockerized Django app and added compose to initiate frontend and backend

--- clothing_compare/start_server.sh
+++ clothing_compare/start_server.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/sh
 scriptDir=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
 port=$1
 
@@ -7,4 +7,5 @@ then
     port=8000
 fi
 
+python3 $scriptDir/manage.py migrate
 python3 $scriptDir/manage.py runserver 0:$port
