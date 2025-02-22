4e7e549ea869c1cb5e3d24905195972696aea8f3
Alejandro Fernandez-Fraga <a.fernandez3@udc.es>
2025-02-22 18:12:37 +0100

    aded upload file

--- frontend/src/home/components/Main.jsx
+++ frontend/src/home/components/Main.jsx
@@ -4,6 +4,7 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 import { useNavigate } from 'react-router-dom';
 import { ReactTyped } from 'react-typed';
 
@@ -37,6 +38,26 @@ const Main = ({imageURL, setImageURL, userName}) => {
   }
   , [capturedImage]);
 
+  function startFromFile(event) {
+    event.preventDefault();
+    const file = event.target[0].files[0];
+    const reader = new FileReader();
+    if (!file) {
+      return;
+    }
+    // if file is not image bye bye
+    if (!file.type.startsWith("image")) {
+      return;
+    }
+    // Set the state to started and the captured image to the file
+    setIsStarted(true);
+    reader.onloadend = function() {
+      setCapturedImage(reader.result);
+    }
+    reader.readAsDataURL(file);
+  }
+
+
   return (
     <main>
       <Container fluid>
@@ -58,21 +79,30 @@ const Main = ({imageURL, setImageURL, userName}) => {
           </Col>
         </Row>
         )}
-        { !isStarted && (
-        <Row>
+        {!isStarted && (
+        <Row md={1} lg={2}>
           <Col>
               <Button onClick={() => setIsStarted(true)} variant="primary">
                 Start 📷
               </Button>
           </Col>
+          <Col>
+            <Form onSubmit={startFromFile}>
+            <Form.Group controlId="formFile">
+              <Form.Label>Upload an image</Form.Label>
+              <Form.Control type="file" size="lg" accept="image/*" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Start  📁
+            </Button>
+            </Form>
+          </Col>
         </Row>
-        )
-        }
+        )}
         {isStarted && (
         <>
               {!capturedImage && <Webcam setCapturedImage={setCapturedImage} />}
               {capturedImage && (
-
                 <img
                   style={{width: "80%", display: "block", margin: "auto", padding: "20px"}}
                   src={capturedImage}
@@ -80,12 +110,11 @@ const Main = ({imageURL, setImageURL, userName}) => {
                   className="captured-image"
                 />
               )}
-
         </>
         )}
         {capturedImage && (
         <Row xs={1} md={1} lg={3}>
-          <Col >
+          <Col>
               <Button onClick={() => {navigate("/compare")}} variant="primary">
                 Compare fabric
               </Button>
@@ -103,7 +132,6 @@ const Main = ({imageURL, setImageURL, userName}) => {
         </Row>
         )}
       </Container>
-
     </main>
   );
 }
