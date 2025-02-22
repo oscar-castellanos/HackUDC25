551a413b2f853346b5a00e237639e75b37fff346
Óscar Castellanos Rodríguez <oscar.castellanos@udc.es>
2025-02-22 02:14:11 +0100

    Setup bind mounts to allow modifying code and seeing changes without rebuilding Docker images

--- clothing_compare/clothing_app/views.py
+++ clothing_compare/clothing_app/views.py
@@ -10,8 +10,7 @@ class ReactView(APIView):
     serializer_class = ReactSerializer
 
     def get(self, request):
-        detail = [ {"name": detail.name,"detail": detail.detail} 
-        for detail in React.objects.all()]
+        detail = [ {"name": detail.name,"detail": detail.detail} for detail in React.objects.all()]
         return Response(detail)
 
     def post(self, request):
