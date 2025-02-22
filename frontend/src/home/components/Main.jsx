9f30ebdf350525e30365efab4fcd0ca7876b5aa6
Alejandro Fernandez-Fraga <a.fernandez3@udc.es>
2025-02-22 19:25:16 +0100

    initial refactoring of main home page

--- frontend/src/home/components/Main.jsx
+++ frontend/src/home/components/Main.jsx
@@ -9,9 +9,23 @@ import { useNavigate } from 'react-router-dom';
 import { ReactTyped } from 'react-typed';
 
 const Main = ({imageURL, setImageURL, userName}) => {
+  const [prompt, setPrompt] = useState("");
   const [capturedImage, setCapturedImage] = useState(null);
   const [isStarted, setIsStarted] = useState(false);
+  const [isHidden, setIsHidden] = useState(true);
+  const [isDoubleHidden, setIsDoubleHidden] = useState(true);
+  const [infoType, setInfoType] = useState("image");
+  
 
+  setTimeout(() => {
+    if (isHidden)
+      setIsHidden(false);
+  }, 1600);
+
+  setTimeout(() => {
+    if (isDoubleHidden)
+      setIsDoubleHidden(false);
+  }, 4000);
 
   const navigate = useNavigate();
 
@@ -57,10 +71,21 @@ const Main = ({imageURL, setImageURL, userName}) => {
     reader.readAsDataURL(file);
   }
 
+  function startFromPrompt(event) {
+    event.preventDefault();
+    const prompt = event.target[0].value;
+    if (!prompt) {
+      return;
+    }
+    setPrompt(prompt);
+    setIsStarted(true);
+    setInfoType("prompt");
+  }
+
 
   return (
     <main>
-      <Container fluid>
+      <Container >
         {!isStarted && (
         <Row>
           <Col>
@@ -71,25 +96,25 @@ const Main = ({imageURL, setImageURL, userName}) => {
         </Row>
         )}
         {!isStarted && (
-        <Row>
+        <Row xxs={1} xs={1} sm={1} md={1} lg={1}>
           <Col>
             <h2>
-              <ReactTyped strings={["Start by taking a photo"]} typeSpeed={100} loop/>
+              <ReactTyped strings={["Start by taking a photo"]} typeSpeed={50} showCursor={false}/>
             </h2>
           </Col>
-        </Row>
-        )}
-        {!isStarted && (
-        <Row md={1} lg={2}>
           <Col>
               <Button onClick={() => setIsStarted(true)} variant="primary">
                 Start 📷
               </Button>
           </Col>
           <Col>
+            <h2>
+              Or {isHidden ? ". . .": (<ReactTyped strings={["uploading a picture of a cool outfit"]} typeSpeed={50} showCursor={false}/>)}
+            </h2>
+          </Col>
+          <Col>
             <Form onSubmit={startFromFile}>
             <Form.Group controlId="formFile">
-              <Form.Label>Upload an image</Form.Label>
               <Form.Control type="file" size="lg" accept="image/*" />
             </Form.Group>
             <Button variant="primary" type="submit">
@@ -97,9 +122,24 @@ const Main = ({imageURL, setImageURL, userName}) => {
             </Button>
             </Form>
           </Col>
+          <Col>
+            <h2>
+              Or {isDoubleHidden ? ". . .": (<ReactTyped strings={["writing a prompt with a style you like "]} typeSpeed={50} showCursor={false}/>)}
+            </h2>
+          </Col>
+          <Col>
+            <Form onSubmit={startFromPrompt}>
+              <Form.Group controlId="formInputPrompt">
+                <Form.Control type="text" placeholder="I like a style with..." />
+              </Form.Group>
+              <Button variant="primary" type="submit">
+                Start 📝
+              </Button>
+            </Form>
+          </Col>
         </Row>
         )}
-        {isStarted && (
+        {isStarted && infoType === 'image' && (
         <>
               {!capturedImage && <Webcam setCapturedImage={setCapturedImage} />}
               {capturedImage && (
@@ -110,6 +150,34 @@ const Main = ({imageURL, setImageURL, userName}) => {
                   className="captured-image"
                 />
               )}
+        </>
+        )}
+        {isStarted && infoType === 'prompt' && (
+        <>
+        <Row>
+          <Col>
+            <h2>
+              You have chosen the prompt: "{prompt}"
+            </h2>
+          </Col>
+        </Row>
+        <Row xs={1} md={1} lg={3}>
+          <Col>
+              <Button onClick={() => {navigate("/compare")}} variant="primary">
+                Compare fabric
+              </Button>
+          </Col>
+          <Col>
+              <Button onClick={() => {navigate("/compare")}} variant="primary">
+                Recomend me a new outfit
+              </Button>
+          </Col>
+          <Col>
+              <Button onClick={() => {setPrompt(""); setIsStarted(false); setInfoType("image");}} variant="danger">
+                Take new Prompt
+              </Button>
+          </Col>
+        </Row>
         </>
         )}
         {capturedImage && (
@@ -125,7 +193,7 @@ const Main = ({imageURL, setImageURL, userName}) => {
               </Button>
           </Col>
           <Col>
-              <Button onClick={() => setCapturedImage(null)} variant="danger">
+              <Button onClick={() => {setCapturedImage(null); setIsStarted(false);}} variant="danger">
                 Take new Image
               </Button>
           </Col>
