50eb44f8f8c62597e15b734a4a0253f7dfc01d9c
Alejandro Fernandez-Fraga <a.fernandez3@udc.es>
2025-02-23 01:13:43 +0100

    Prompt search component formated and pulished

--- frontend/src/compare/components/PromptSearch.jsx
+++ frontend/src/compare/components/PromptSearch.jsx
@@ -1,47 +1,101 @@
-import { useState, useEffect } from 'react';
-import { ClothingList } from '../../common';
-import Spinner from 'react-bootstrap/Spinner';
-import { OutfitSearch } from '../../search';
+import { useState, useEffect } from "react";
+import { ClothingList } from "../../common";
+import Spinner from "react-bootstrap/Spinner";
+import { OutfitSearch } from "../../search";
+import Container from "react-bootstrap/esm/Container";
+import { ReactTyped } from "react-typed";
 
-const PromptSearch = ({prompt}) => {
-    const backURL = 'http://localhost:8000/clothing_app/prompt_search';
+const PromptSearch = ({ prompt }) => {
+  const backURL = "http://localhost:8000/clothing_app/prompt_search";
 
-    const [clothData, setClothData] = useState(null);
-    const [currentClothingDetail, setCurrentClothingDetail] = useState(null);
+  const [clothData, setClothData] = useState(null);
+  const [currentClothingDetail, setCurrentClothingDetail] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
-    useEffect(() => {
-        if (!prompt) {
+  useEffect(() => {
+    if (!prompt) {
+      return;
+    }
+    fetch(backURL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ description: prompt }),
+    })
+      .then((response) => {
+        console.log("Status:", response.status);
+        return response.json();
+      })
+      .then((data) => {
+        console.log("Success:", data);
+        if (data) {
+          if (data.length === 0) {
+            setIsLoading(false);
             return;
+          }
+          if (clothData && data.length < clothData.length) {
+            return;
+          }
+          setClothData(data);
+          setIsLoading(false);
         }
-        fetch(backURL, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ description: prompt }),
-        })
-            .then((response) => {console.log("Status:", response.status); return response.json()})
-            .then((data) => {
-                console.log('Success:', data);
-                setClothData(data);
-            });
-    }, []);
+      });
+  }, []);
 
-
-    return (
-        <>
-        <h2>Get clothes from prompt = {prompt}</h2>
-        {!clothData && (
-                  <Spinner style={{position: 'absolute', top: '50%', left: '50%', width: '5rem', height: '5rem'}} animation="border" />
-                )}
-              {(clothData && !currentClothingDetail)  && (
-                <ClothingList clothing={clothData} setCurrentClothingDetail={setCurrentClothingDetail}/>
-              )}
-              {currentClothingDetail && (
-                <OutfitSearch currentClothingDetail={currentClothingDetail} setCurrentClothingDetail={setCurrentClothingDetail}/>
-            )}
-        </>
-        );
-}
+  return (
+    <>
+      <Container fluid className="text-center">
+        <h2>
+          {isLoading ? (
+            <ReactTyped
+              strings={[
+                "Loading...",
+                "Please wait...",
+                "Getting things ready...",
+                "Almost there...",
+                "Processing...",
+              ]}
+              typeSpeed={30}
+              showCursor={false}
+              loop
+            />
+          ) : (
+            `Prompt analysed!`
+          )}
+        </h2>
+      </Container>
+      {!clothData && isLoading && (
+        <Spinner
+          style={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            width: "5rem",
+            height: "5rem",
+          }}
+          animation="border"
+        />
+      )}
+      {!clothData && !isLoading && (
+        <Container fluid className="text-center">
+          <h3>No clothing found for the current image!</h3>
+        </Container>
+      )}
+      {clothData && !currentClothingDetail && (
+        <ClothingList
+          clothing={clothData}
+          setCurrentClothingDetail={setCurrentClothingDetail}
+        />
+      )}
+      {currentClothingDetail && (
+        <OutfitSearch
+          currentClothingDetail={currentClothingDetail}
+          setCurrentClothingDetail={setCurrentClothingDetail}
+        />
+      )}
+    </>
+  );
+};
 
 export default PromptSearch;
