2aaccdaf24aa118e3cbf950480c743396322d27a
Alejandro Fernandez-Fraga <a.fernandez3@udc.es>
2025-02-23 01:03:13 +0100

    Formated Main component

--- frontend/src/home/components/Main.jsx
+++ frontend/src/home/components/Main.jsx
@@ -1,30 +1,27 @@
-import { useState, useEffect } from 'react';
-import Webcam from './Webcam';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import { useNavigate } from 'react-router-dom';
-import { ReactTyped } from 'react-typed';
-import Fade from 'react-bootstrap/Fade';
+import { useState, useEffect } from "react";
+import Webcam from "./Webcam";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import { useNavigate } from "react-router-dom";
+import { ReactTyped } from "react-typed";
+import Fade from "react-bootstrap/Fade";
 
-const Main = ({imageURL, setImageURL, userName, prompt, setPrompt}) => {
+const Main = ({ imageURL, setImageURL, userName, prompt, setPrompt }) => {
   const [capturedImage, setCapturedImage] = useState(null);
   const [isStarted, setIsStarted] = useState(false);
   const [isHidden, setIsHidden] = useState(true);
   const [isDoubleHidden, setIsDoubleHidden] = useState(true);
   const [infoType, setInfoType] = useState("image");
-  
 
   setTimeout(() => {
-    if (isHidden)
-      setIsHidden(false);
+    if (isHidden) setIsHidden(false);
   }, 1600);
 
   setTimeout(() => {
-    if (isDoubleHidden)
-      setIsDoubleHidden(false);
+    if (isDoubleHidden) setIsDoubleHidden(false);
   }, 4000);
 
   const navigate = useNavigate();
@@ -35,22 +32,19 @@ const Main = ({imageURL, setImageURL, userName, prompt, setPrompt}) => {
       //console.log("Captured image changed");
       //console.log("Captured image: ", capturedImage);
       fetch(`http://localhost:8000/clothing_app/user_clothing/${userName}`, {
-        method: 'POST',
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
-        body: JSON.stringify({ user: userName,
-                                image_string: capturedImage}),
+        body: JSON.stringify({ user: userName, image_string: capturedImage }),
       })
         .then((response) => response.json())
         .then((data) => {
           //console.log('Success:', data);
-          if (data.image_url)
-            setImageURL(data.image_url);
+          if (data.image_url) setImageURL(data.image_url);
         });
     }
-  }
-  , [capturedImage]);
+  }, [capturedImage]);
 
   function startFromFile(event) {
     event.preventDefault();
@@ -65,9 +59,9 @@ const Main = ({imageURL, setImageURL, userName, prompt, setPrompt}) => {
     }
     // Set the state to started and the captured image to the file
     setIsStarted(true);
-    reader.onloadend = function() {
+    reader.onloadend = function () {
       setCapturedImage(reader.result);
-    }
+    };
     reader.readAsDataURL(file);
   }
 
@@ -82,136 +76,186 @@ const Main = ({imageURL, setImageURL, userName, prompt, setPrompt}) => {
     setInfoType("prompt");
   }
 
-
   return (
     <main>
-      <Container >
+      <Container>
         {!isStarted && (
-        <Row >
-          <Col>
-            <h3 className="mt-3 mb-5" style={{textAlign: "center"}}>
-              Welcome to
-            </h3>
-            <Fade in={true} appear={true}>
-              <h1 className="mt-3 mb-3" style={{textAlign: "center"}}>
-                Clothing compare
-              </h1>
-            </Fade>
-            <h4 className='mt-3' style={{textAlign: "center"}}>
-              Find sustaiable and skin-friendly clothing that fits your style
-            </h4>
-          </Col>
-        </Row>
-        )}
-        {!isStarted && (
-        <div className="shadow-sm p-3 mb-5 bg-white rounded">
-          <Row xxs={1} xs={1} sm={1} md={1} lg={1}>
+          <Row>
             <Col>
-              <h4 className="mt-5 mb-3" style={{textAlign: "center"}}>
-                <ReactTyped strings={["Start taking a photo"]} typeSpeed={50} showCursor={false}/>
+              <h3 className="mt-3 mb-5" style={{ textAlign: "center" }}>
+                Welcome to
+              </h3>
+              <Fade in={true} appear={true}>
+                <h1 className="mt-3 mb-3" style={{ textAlign: "center" }}>
+                  Clothing compare
+                </h1>
+              </Fade>
+              <h4 className="mt-3" style={{ textAlign: "center" }}>
+                Find sustaiable and skin-friendly clothing that fits your style
               </h4>
-            </Col>
-            <Col>
-                <Button style={{display: "flex", textAlign: "center"}} size="lg" onClick={() => setIsStarted(true)} variant="primary">
-                  Start 📷  
-                </Button>
-            </Col>
-            <Col>
-              <h4 className="mt-4" style={{textAlign: "center"}}>
-                or {isHidden ? ". . .": (<ReactTyped strings={["uploading a picture of a cool outfit"]} typeSpeed={50} showCursor={false}/>)}
-              </h4>
-            </Col>
-            <Col>
-              <Form onSubmit={startFromFile}>
-              <Form.Group controlId="formFile">
-                <Form.Control type="file" size="lg" accept="image/*" />
-              </Form.Group>
-              <Button variant="primary" type="submit" style={{align: "center"}} size="lg">
-                Start  📁
-              </Button>
-              </Form>
-            </Col>
-            <Col>
-              <h4 className ="mt-4" style={{textAlign: "center"}}>
-                or {isDoubleHidden ? ". . .": (<ReactTyped strings={["describing a style you like"]} typeSpeed={50} showCursor={false}/>)}
-              </h4>
-            </Col>
-            <Col>
-              <Form onSubmit={startFromPrompt}>
-                <Form.Group controlId="formInputPrompt">
-                  <Form.Control type="text" placeholder="I'm looking for an outfit with..." />
-                </Form.Group>
-                <Button variant="primary" type="submit" size="lg">
-                  Start 📝
-                </Button>
-              </Form>
             </Col>
           </Row>
-        </div>
         )}
-        {isStarted && infoType === 'image' && (
-        <>
-              {!capturedImage && <Webcam setCapturedImage={setCapturedImage} />}
-              {capturedImage && (
-                <img
-                  style={{height: "80%", display: "block", margin: "auto", padding: "20px"}}
-                  src={capturedImage}
-                  alt="Captured"
-                  className="captured-image"
-                />
-              )}
-        </>
+        {!isStarted && (
+          <div className="shadow-sm p-3 mb-5 bg-white rounded">
+            <Row xxs={1} xs={1} sm={1} md={1} lg={1}>
+              <Col>
+                <h4 className="mt-5 mb-3" style={{ textAlign: "center" }}>
+                  <ReactTyped
+                    strings={["Start taking a photo"]}
+                    typeSpeed={50}
+                    showCursor={false}
+                  />
+                </h4>
+              </Col>
+              <Col>
+                <Button
+                  style={{ display: "flex", textAlign: "center" }}
+                  size="lg"
+                  onClick={() => setIsStarted(true)}
+                  variant="primary"
+                >
+                  Start 📷
+                </Button>
+              </Col>
+              <Col>
+                <h4 className="mt-4" style={{ textAlign: "center" }}>
+                  or{" "}
+                  {isHidden ? (
+                    ". . ."
+                  ) : (
+                    <ReactTyped
+                      strings={["uploading a picture of a cool outfit"]}
+                      typeSpeed={50}
+                      showCursor={false}
+                    />
+                  )}
+                </h4>
+              </Col>
+              <Col>
+                <Form onSubmit={startFromFile}>
+                  <Form.Group controlId="formFile">
+                    <Form.Control type="file" size="lg" accept="image/*" />
+                  </Form.Group>
+                  <Button
+                    variant="primary"
+                    type="submit"
+                    style={{ align: "center" }}
+                    size="lg"
+                  >
+                    Start 📁
+                  </Button>
+                </Form>
+              </Col>
+              <Col>
+                <h4 className="mt-4" style={{ textAlign: "center" }}>
+                  or{" "}
+                  {isDoubleHidden ? (
+                    ". . ."
+                  ) : (
+                    <ReactTyped
+                      strings={["describing what you are looking for"]}
+                      typeSpeed={50}
+                      showCursor={false}
+                    />
+                  )}
+                </h4>
+              </Col>
+              <Col>
+                <Form onSubmit={startFromPrompt}>
+                  <Form.Group controlId="formInputPrompt">
+                    <Form.Control
+                      type="text"
+                      placeholder="I'm looking for an outfit with..."
+                    />
+                  </Form.Group>
+                  <Button variant="primary" type="submit" size="lg">
+                    Start 📝
+                  </Button>
+                </Form>
+              </Col>
+            </Row>
+          </div>
         )}
-        {isStarted && infoType === 'prompt' && (
-        <>
-        <Row>
-          <Col>
-            <h2>
-              You have chosen the prompt: "{prompt}"
-            </h2>
-          </Col>
-        </Row>
-        <Row xs={1} md={1} lg={3}>
-          <Col>
-              <Button onClick={() => {navigate("/promptSearch")}} variant="primary">
-                Compare fabric
-              </Button>
-          </Col>
-          <Col>
-              <Button onClick={() => {navigate("/search")}} variant="primary">
-                Recomend me a new outfit
-              </Button>
-          </Col>
-          <Col>
-              <Button onClick={() => {setPrompt(""); setIsStarted(false); setInfoType("image");}} variant="danger">
-                Take new Prompt
-              </Button>
-          </Col>
-        </Row>
-        </>
+        {isStarted && infoType === "image" && (
+          <>
+            {!capturedImage && <Webcam setCapturedImage={setCapturedImage} />}
+            {capturedImage && (
+              <img
+                style={{
+                  height: "80%",
+                  display: "block",
+                  margin: "auto",
+                  padding: "20px",
+                }}
+                src={capturedImage}
+                alt="Captured"
+                className="captured-image"
+              />
+            )}
+          </>
+        )}
+        {isStarted && infoType === "prompt" && (
+          <>
+            <Row>
+              <Col>
+                <h2>You are looking for: "{prompt}"</h2>
+              </Col>
+            </Row>
+            <Row xs={1} md={1} lg={2}>
+              <Col>
+                <Button
+                  onClick={() => {
+                    navigate("/promptSearch");
+                  }}
+                  variant="primary"
+                >
+                  Find items
+                </Button>
+              </Col>
+              <Col>
+                <Button
+                  onClick={() => {
+                    setPrompt("");
+                    setIsStarted(false);
+                    setInfoType("image");
+                  }}
+                  variant="danger"
+                >
+                  Discard Prompt
+                </Button>
+              </Col>
+            </Row>
+          </>
         )}
         {capturedImage && (
-        <Row xs={1} md={1} lg={3}>
-          <Col>
-              <Button onClick={() => {navigate("/compare")}} variant="primary">
-                Compare fabric
+          <Row xs={1} md={1} lg={2} className="mt-3 center mx-auto">
+            <Col>
+              <Button
+                onClick={() => {
+                  navigate("/compare");
+                }}
+                variant="primary"
+              >
+                Find similar items
               </Button>
-          </Col>
-          <Col>
-              <Button onClick={() => {navigate("/search")}} variant="primary">
-                Recomend me a new outfit
+            </Col>
+            <Col>
+              <Button
+                onClick={() => {
+                  setCapturedImage(null);
+                  setIsStarted(false);
+                }}
+                variant="danger"
+              >
+                Discard Image
               </Button>
-          </Col>
-          <Col>
-              <Button onClick={() => {setCapturedImage(null); setIsStarted(false);}} variant="danger">
-                Take new Image
-              </Button>
-          </Col>
-        </Row>
+            </Col>
+          </Row>
         )}
       </Container>
     </main>
   );
-}
+};
 
 export default Main;
