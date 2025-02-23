ea24f71247d9ae06e0457456b5bd91f7db7e9649
Alejandro Fernandez-Fraga <a.fernandez3@udc.es>
2025-02-23 02:17:06 +0100

    pullished Output Search view

--- frontend/src/search/components/OutfitSearch.jsx
+++ frontend/src/search/components/OutfitSearch.jsx
@@ -1,11 +1,12 @@
-import Container from 'react-bootstrap/Container';
-import {useState, useEffect} from 'react';
-import { OutfitDetails } from '../../common';
+import Container from "react-bootstrap/Container";
+import { useState, useEffect } from "react";
+import { OutfitDetails } from "../../common";
+import Spinner from "react-bootstrap/Spinner";
+import { ReactTyped } from "react-typed";
 
-
-const OutfitSearch = ({currentClothingDetail, setCurrentClothingDetail}) => {
-
+const OutfitSearch = ({ currentClothingDetail, setCurrentClothingDetail }) => {
   const [outfitData, setOutfitData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   // fetch a POST
   // TO URL: http://localhost:8000/clothing_app/visual_search/outfit_search
@@ -16,38 +17,89 @@ const OutfitSearch = ({currentClothingDetail, setCurrentClothingDetail}) => {
       return;
     }
     console.log("Current clothing detail: ", currentClothingDetail);
-    fetch('http://localhost:8000/clothing_app/visual_search/outfit_search', {
-      method: 'POST',
+    fetch("http://localhost:8000/clothing_app/visual_search/outfit_search", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify(currentClothingDetail),
     })
-      .then((response) => {console.log("Status:", response.status); return response.json()})
+      .then((response) => {
+        console.log("Status:", response.status);
+        return response.json();
+      })
       .then((data) => {
-        console.log('Success:', data);
-        setOutfitData(data);
+        console.log("Success:", data);
+        if (data) {
+          if (data.length === 0) {
+            setIsLoading(false);
+            return;
+          }
+          if (outfitData && data.length <= outfitData.length) {
+            return;
+          }
+          setOutfitData(data);
+          setIsLoading(false);
+        }
       });
   }, []);
 
-
   return (
     <>
-    <h2>Outfit Search</h2>
-    <h3>Current clothing detail {JSON.stringify(currentClothingDetail, null, 2)}</h3>
-    {!outfitData && (
-      <p>Loading outfit data...</p>
-    )}
-    {outfitData && (
-      <Container>
-      <p>Outfit data loaded</p>
-      {
-        outfitData.map((outfit, index) => (
-          <OutfitDetails key={index} outfit={outfit.outfit_parts}/>
-        ))
-      }
+      <Container fluid className="text-center mt-5 mb-4">
+        <h2>
+          {isLoading ? (
+            <ReactTyped
+              strings={[
+                "Booting up the system...",
+                "Compiling code...",
+                "Decrypting mainframe...",
+                "Optimizing algorithms...",
+                "Syncing data streams...",
+                "Calibrating sensors...",
+                "Patching the matrix...",
+                "Initializing quantum core...",
+                "Executing startup sequence...",
+                "Charging flux capacitor...",
+                "Connecting to the cloud...",
+                "Rendering in hyper-speed...",
+                "Activating AI protocols...",
+                "Enhancing neural networks...",
+                "Bribing the progress bar...",
+              ]}
+              typeSpeed={30}
+              showCursor={false}
+              loop
+            />
+          ) : (
+            `Check your proposed outfits!`
+          )}
+        </h2>
       </Container>
-    )}
+      {isLoading && (
+        <Spinner
+          style={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            width: "5rem",
+            height: "5rem",
+          }}
+          animation="border"
+        />
+      )}
+      {!outfitData && !isLoading && (
+        <Container fluid className="text-center">
+          <h3>No outfits found for the current item!</h3>
+        </Container>
+      )}
+      {outfitData && (
+        <Container>
+          {outfitData.map((outfit, index) => (
+            <OutfitDetails key={index} outfit={outfit.outfit_parts} />
+          ))}
+        </Container>
+      )}
     </>
   );
 };
